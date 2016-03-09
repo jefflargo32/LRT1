09b37554533e354f07d2d1001eee9a3bc4252f37
Jeff Ray L. Largo <jefflargo32@yahoo.com>
2016-03-09 14:28:19 +0800

    HelloWorld using Visual Basic

---
+++ HelloWorld/HelloWorld/Form1.Designer.vb
@@ -0,0 +1,55 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class HelloWorldFrom
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(HelloWorldFrom))
+        Me.btnHW = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnHW
+        '
+        Me.btnHW.Font = New System.Drawing.Font("Franklin Gothic Demi", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnHW.Location = New System.Drawing.Point(67, 56)
+        Me.btnHW.Name = "btnHW"
+        Me.btnHW.Size = New System.Drawing.Size(115, 32)
+        Me.btnHW.TabIndex = 0
+        Me.btnHW.Text = "Hello World"
+        Me.btnHW.UseVisualStyleBackColor = True
+        '
+        'HelloWorldFrom
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(246, 127)
+        Me.Controls.Add(Me.btnHW)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "HelloWorldFrom"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Hello World"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnHW As System.Windows.Forms.Button
+
+End Class
---
+++ HelloWorld/HelloWorld/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class HelloWorldFrom
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnHW.Click
+        MessageBox.Show("Hello World!", "HelloWorld", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Application.Exit()
+    End Sub
+End Class
